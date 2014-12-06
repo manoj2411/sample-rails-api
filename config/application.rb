2e8223a69814a6a0f5c2e68d0edec5ba3e86f922
Manoj <manoj.delhi24@gmail.com>
2014-12-07 03:21:44 +0530

    disabled few middlewares

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,14 @@ module ApiOnlyApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.delete ActionDispatch::Static
+    # config.middleware.delete Rack::Runtime
+    config.middleware.delete Rails::Rack::Logger
+    config.middleware.delete ActionDispatch::Reloader
+    config.middleware.delete Rack::ConditionalGet
+    config.middleware.delete Rack::ConditionalGet
+    config.middleware.delete Rack::ETag
+
   end
 end
